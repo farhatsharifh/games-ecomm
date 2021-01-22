00c7983f22ef87aff7c3abb34981a004e38edf8c
Farhat Sharif <farhatsharif.it@gmail.com>
2021-01-22 09:45:29 +0500

    Create home component, Add home page banner, Add seperate module for Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
+import { AppRoutingModule } from './app-routing.module';
+import { AngularMaterialModule } from './ecomm-modules/angular-material.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     NgbModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule
+    AngularMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
