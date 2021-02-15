b76a8ff9830b1b8fd0c37285c25cd99e92605edc
Farhat Sharif <farhatsharif.it@gmail.com>
2021-02-16 02:33:29 +0500

    Create Product model

---
+++ src/app/products/product.model.ts
@@ -0,0 +1,10 @@
+export interface Product {
+  id: string;
+  title: string;
+  price: string;
+  description: string;
+  imagePath: string;
+  detail: string;
+  category: string;
+  featured: boolean;
+}
